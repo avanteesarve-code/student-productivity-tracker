f368f369f3a7b76f1d61f29d91494be347e9e40c
Avantee Sarve <avanteesarve@gmail.com>
2026-02-18 15:21:08 +0530

    Added Bar Charts showing total study hours by subject using Chart.js

---
+++ force-app/main/default/lwc/studySessionChart/studySessionChart.js
@@ -0,0 +1,69 @@
+import { LightningElement, wire } from 'lwc';
+import getStudyHoursBySubject from '@salesforce/apex/StudySessionController.getStudyHoursBySubject';
+import { loadScript } from 'lightning/platformResourceLoader';
+import ChartJS from '@salesforce/resourceUrl/ChartJS';
+
+export default class StudySessionChart extends LightningElement {
+
+    chart;
+    chartJsInitialized = false;
+    chartData;
+
+    @wire(getStudyHoursBySubject)
+    wiredData({ data, error }) {
+        if (data) {
+            this.chartData = data;
+            this.renderChart();
+        } else if (error) {
+            console.error(error);
+        }
+    }
+
+    renderedCallback() {
+        if (this.chartJsInitialized) {
+            return;
+        }
+
+        this.chartJsInitialized = true;
+
+        loadScript(this, ChartJS)
+            .then(() => {
+                if (this.chartData) {
+                    this.renderChart();
+                }
+            })
+            .catch(error => {
+                console.error('ChartJS load error:', error);
+            });
+    }
+
+   renderChart() {
+
+    if (!this.chartJsInitialized || !this.chartData) {
+        return;
+    }
+
+    const canvas = this.template.querySelector('canvas');
+    if (!canvas) return;
+
+    const ctx = canvas.getContext('2d');
+
+    // Convert Map → Array
+    const labels = Object.keys(this.chartData);
+    const values = Object.values(this.chartData);
+
+    if (this.chart) {
+        this.chart.destroy();
+    }
+
+    this.chart = new window.Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Total Study Hours',
+                data: values
+            }]
+        }
+    });
+}}
